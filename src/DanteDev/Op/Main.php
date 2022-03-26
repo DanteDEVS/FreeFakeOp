9786b6d635854fe4fb83955cbe463668c1d44e28
testdev <83214438+DanteDEVS@users.noreply.github.com>
2022-03-26 15:58:53 +0200

    the basics of a plugin

--- src/DanteDev/Op/Main.php
+++ src/DanteDev/Op/Main.php
@@ -1,1 +1,20 @@
+<?php
 
+namespace DanteDev\Op;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+  
+  public function onLoad(): void{
+      $this->getLogger()->info("Loading FakeOP");
+  }
+  
+  public function onEnable(): void{
+      $this->getLogger()->info("Enabled FakeOP");
+  }
+  
+  public function onLoad(): void{
+      $this->getLogger()->info("Disabled FakeOP");
+  }
+}
